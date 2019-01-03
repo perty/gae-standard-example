98668996b8aa427ed6a0590f3f0d8afab96417ad
Per Lundholm <per@artcomputer.se>
2019-01-03 21:00:38 +0100

    Added scripts for local datastore

---
+++ start-local-datastore-browser.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+$(gcloud beta emulators datastore env-init)
+dsui
---
+++ start-local-datastore.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+gcloud beta emulators datastore start --data-dir=target/demo-0.0.1-SNAPSHOT
